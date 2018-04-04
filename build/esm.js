99a57217a21a604d95274c0fc6d86dd978bc4149
Azin Asili <azinasili@gmail.com>
2018-04-04 02:11:37 -0400

    simplified build process

--- build/esm.js
+++
@@ -1,9 +0,0 @@
-import build from './build';
-
-export default Object.assign(build, {
-  input: 'src/a11ytrap.js',
-  output: Object.assign(build.output, {
-    file: 'dist/a11ytrap.esm.js',
-    format: 'es',
-  }),
-});
